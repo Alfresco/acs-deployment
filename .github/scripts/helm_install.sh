73d53c36c6184258eb449e10bfc10a3f736fc715
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2022-08-02 11:56:30 +0200

    Try KinD

--- .github/scripts/helm_install.sh
+++ .github/scripts/helm_install.sh
@@ -34,7 +34,7 @@ VALID_VERSION=$(echo "${ACS_VERSION}" | tr -d '.' | awk '{print tolower($0)}')
 namespace=$(echo "${BRANCH_NAME}" | cut -c1-28 | tr /_ - | tr -d [:punct:] | awk '{print tolower($0)}')-"${GITHUB_RUN_NUMBER}"-"${VALID_VERSION}"
 release_name_ingress=ing-"${GITHUB_RUN_NUMBER}"-"${VALID_VERSION}"
 release_name_acs=acs-"${GITHUB_RUN_NUMBER}"-"${VALID_VERSION}"
-HOST=${namespace}.${DOMAIN}
+HOST=localhost
 PROJECT_NAME=alfresco-content-services
 
 # pod status
@@ -189,12 +189,6 @@ helm upgrade --install "${release_name_ingress}" --repo https://kubernetes.githu
   --set rbac.create=true \
   --set controller.config."proxy-body-size"="100m" \
   --set controller.service.targetPorts.https=80 \
-  --set controller.service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-backend-protocol"="http" \
-  --set controller.service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-ssl-ports"="https" \
-  --set controller.service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-ssl-cert"="${ACM_CERTIFICATE}" \
-  --set controller.service.annotations."external-dns\.alpha\.kubernetes\.io/hostname"="${HOST}" \
-  --set controller.service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-ssl-negotiation-policy"="ELBSecurityPolicy-TLS-1-2-2017-01" \
-  --set controller.service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-security-groups"="${AWS_SG}" \
   --set controller.publishService.enabled=true \
   --set controller.admissionWebhooks.enabled=false \
   --set controller.ingressClassResource.enabled=false \
@@ -206,8 +200,8 @@ helm dep up helm/"${PROJECT_NAME}"
 helm upgrade --install "${release_name_acs}" helm/"${PROJECT_NAME}" \
   --values="${values_file}" \
   --set global.tracking.sharedsecret="$(openssl rand -hex 24)" \
-  --set externalPort="443" \
-  --set externalProtocol="https" \
+  --set externalPort="80" \
+  --set externalProtocol="http" \
   --set externalHost="${HOST}" \
   --set persistence.enabled=true \
   --set persistence.storageClass.enabled=true \
@@ -219,27 +213,6 @@ helm upgrade --install "${release_name_acs}" helm/"${PROJECT_NAME}" \
   --timeout 20m0s \
   --namespace="${namespace}"
 
-# check dns and pods
-DNS_PROPAGATED=0
-DNS_COUNTER=0
-DNS_COUNTER_MAX=90
-DNS_SLEEP_SECONDS=10
-
-echo "Trying to perform a trace DNS query to prevent caching"
-dig +trace "${HOST}" @8.8.8.8
-while [ "${DNS_PROPAGATED}" -eq 0 ] && [ "${DNS_COUNTER}" -le "${DNS_COUNTER_MAX}" ]; do
-  host "${HOST}" 8.8.8.8
-  if [ "$?" -eq 1 ]; then
-    DNS_COUNTER=$((DNS_COUNTER + 1))
-    echo "DNS Not Propagated - Sleeping ${DNS_SLEEP_SECONDS} seconds"
-    sleep "${DNS_SLEEP_SECONDS}"
-  else
-    echo "DNS Propagated"
-    DNS_PROPAGATED=1
-  fi
-done
-
-[ "${DNS_PROPAGATED}" -ne 1 ] && echo "DNS entry for ${HOST} did not propagate within expected time" && exit 1
 
 pods_ready || exit 1
 
@@ -249,7 +222,7 @@ sleep 180
 
 # run acs checks
 wait_for_connection
-newman run helm/acs-test-helm-collection.json --global-var "protocol=https" --global-var "url=${HOST}"
+newman run helm/acs-test-helm-collection.json --global-var "protocol=http" --global-var "url=${HOST}"
 TEST_RESULT=$?
 echo "TEST_RESULT=${TEST_RESULT}"
 if [[ "${TEST_RESULT}" == "0" ]]; then
@@ -279,7 +252,7 @@ if [[ "${TEST_RESULT}" == "0" ]]; then
 
     # run checks after pod deletion
     wait_for_connection
-    newman run "helm/acs-validate-volume-collection.json" --global-var "protocol=https" --global-var "url=${HOST}"
+    newman run "helm/acs-validate-volume-collection.json" --global-var "protocol=http" --global-var "url=${HOST}"
     TEST_RESULT=$?
     echo "TEST_RESULT=${TEST_RESULT}"
   fi
