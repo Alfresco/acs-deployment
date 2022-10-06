a935865307b810912cae76f6fca0258dad22a610
Alexandre Chapellon <alexandre.chapellon@hyland.com>
2022-10-06 17:29:16 +0200

    allow skip checking out the branch

--- .github/actions/use-local-deps/local-deps.sh
+++ .github/actions/use-local-deps/local-deps.sh
@@ -1,10 +1,10 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 # Compute charts dependencies and force local dependencies resolution
 CHARTS_ROOT=$(dirname $1)
 CHART_NAME=$(basename $1)
 
-for DEP_ROOT in $(ls -d "${CHARTS_ROOT}/*" | grep -v "${CHART_NAME}")
-do DEP=$(basename $DEP_ROOT)
+for DEP_ROOT in $(ls -d "${CHARTS_ROOT}"/* | grep -v "${CHART_NAME}")
+do export DEP=$(basename $DEP_ROOT)
   echo "Looking for $DEP as a dependency of $CHART_NAME"
   DEP_VER=$(yq e '.version' ${CHARTS_ROOT}/${DEP}/Chart.yaml)
   if yq e -i 'del(.dependencies[] | select(.name==strenv(DEP)).repository)' "${CHARTS_ROOT}/${CHART_NAME}/Chart.yaml" > /dev/null 2>&1; then
